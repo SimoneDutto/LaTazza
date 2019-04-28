355ccdcd2bfddc51a230dd298ebd669a4a500589
Debora Caldarola <s263626@studenti.polito.it>
2019-04-28 12:21:58 +0000

    Update BeverageException.java

--- src/main/java/it/polito/latazza/exceptions/BeverageException.java
+++ src/main/java/it/polito/latazza/exceptions/BeverageException.java
@@ -3,5 +3,8 @@ package it.polito.latazza.exceptions;
 public class BeverageException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-
+	
+	public BeverageException(String message) {
+		super(message);
+	}
 }
