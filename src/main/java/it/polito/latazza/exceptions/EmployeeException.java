cc05138a8bf64a68fcad8ec225412a81ca8579f2
Debora Caldarola <s263626@studenti.polito.it>
2019-04-28 12:22:51 +0000

    Update EmployeeException.java

--- src/main/java/it/polito/latazza/exceptions/EmployeeException.java
+++ src/main/java/it/polito/latazza/exceptions/EmployeeException.java
@@ -3,5 +3,8 @@ package it.polito.latazza.exceptions;
 public class EmployeeException extends Exception {
 
 	private static final long serialVersionUID = 1L;
-
+    
+    public EmployeeException(String message) {
+		super(message);
+	}
 }
