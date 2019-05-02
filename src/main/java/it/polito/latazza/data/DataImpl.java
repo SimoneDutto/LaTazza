4dc206cf2577daec65886b3b53da1ff1a1279ee5
Vito Tassielli <s263006@studenti.polito.it>
2019-05-02 18:07:04 +0000

    Update DataImpl.java

--- src/main/java/it/polito/latazza/data/DataImpl.java
+++ src/main/java/it/polito/latazza/data/DataImpl.java
@@ -17,34 +17,88 @@ public class DataImpl implements DataInterface {
 	@Override
 	public Integer sellCapsules(Integer employeeId, Integer beverageId, Integer numberOfCapsules, Boolean fromAccount)
 			throws EmployeeException, BeverageException, NotEnoughCapsules {
+		int balance = DataBase.getInstance().sellCap(employeeId, beverageId, numberOfCapsules, fromAccount); 
 		
-		return 0;
+		if(balance == -3) {
+			throw new EmployeeException("ID of the employee is not valid");
+		}
+		else if(balance == -1) {
+			throw new BeverageException("ID of the beverage is not valid");
+		}
+		else if(balance == -2) {
+			throw new NotEnoughCapsules("Number of available capsules is insufficient");
+		}
+		else {
+			System.out.println("Sell correctly updated");
+			return balance;
+		}
 	}
 
 	@Override
 	public void sellCapsulesToVisitor(Integer beverageId, Integer numberOfCapsules)
 			throws BeverageException, NotEnoughCapsules {
-		// TODO Auto-generated method stub
+		int value = DataBase.getInstance().sellVis(beverageId, numberOfCapsules); 
+
+		if(value == -2) {
+			throw new BeverageException("ID of the beverage is not valid");
+		}
+		else if(value == -1) {
+			throw new NotEnoughCapsules("Number of available capsules is insufficient");
+		}
+		else {
+			System.out.println("Sell correctly updated");
+		}
 		
 	}
 
 	@Override
 	public Integer rechargeAccount(Integer id, Integer amountInCents) throws EmployeeException {
-		// TODO Auto-generated method stub
-		return 0;
+		int value = DataBase.getInstance().recharge(id, amountInCents);
+
+		if(value == -1) {
+			throw new EmployeeException("ID of the employee is not valid");
+		}
+		else {
+			System.out.println("Recharge correctly inserted");
+			return value;
+		}
 	}
 
 	@Override
 	public void buyBoxes(Integer beverageId, Integer boxQuantity) throws BeverageException, NotEnoughBalance {
-		// TODO Auto-generated method stub
+		int value = DataBase.getInstance().buyB(beverageId, boxQuantity);
+
+		if(value == -2) {
+			throw new BeverageException("ID of the beverage is not valid");
+		}
+		else if(value == -1) {
+			throw new NotEnoughBalance("Balance is insufficient");
+		}
+		else {
+			System.out.println("Boxes correctly received and paid for");
+		}
 		
 	}
 
 	@Override
 	public List<String> getEmployeeReport(Integer employeeId, Date startDate, Date endDate)
 			throws EmployeeException, DateException {
-		// TODO Auto-generated method stub
-		return new ArrayList<String>();
+		
+		int emp = DataBase.getInstance().checkEmp(employeeId);
+		List<String> value = new ArrayList<String>();
+		
+		if(emp == -1) {
+			throw new EmployeeException("ID of the employee is not valid");
+		}
+		else if (startDate.after(endDate) == true) {
+			throw new DateException("Date interval is not valid");
+		}
+		
+		else {
+			//value = DataBase.getInstance().getEmplRep(employeeId, startDate, endDate);
+			System.out.println("Report correctly delivered");
+		}
+		return value;
 	}
 
 	@Override
