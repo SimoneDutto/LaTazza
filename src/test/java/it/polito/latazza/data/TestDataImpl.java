1e7b6e6a62d81ab6904e2bac753af71bce3cff55
s257348@studenti.polito.it <s257348@studenti.polito.it>
2019-05-11 18:28:29 +0200

    Add Unit Test for sellCapsules()

--- src/test/java/it/polito/latazza/data/TestDataImpl.java
+++ src/test/java/it/polito/latazza/data/TestDataImpl.java
@@ -1,15 +1,14 @@
 package it.polito.latazza.data;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import it.polito.latazza.exceptions.*;
 
 public class TestDataImpl {
-
-	@Test
-	public void testExample() {
-		assertEquals(2, 1 + 1);
-
-	}
+	// TEST 
+	
+	
 
 }
