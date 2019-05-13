15471fc34cba7041de73a2b0a1876d2c805e20fc
Isabella Romita <s257269@studenti.polito.it>
2019-05-13 11:23:07 +0000

    Add TestRecharge.java

---
+++ src/test/java/it/polito/latazza/data/TestRecharge.java
@@ -0,0 +1,83 @@
+package it.polito.latazza.data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import it.polito.latazza.data.DataImpl;
+import it.polito.latazza.exceptions.EmployeeException;
+
+public class TestRecharge {
+
+	@Test
+	public void testRechargeBalance() throws EmployeeException{
+		
+		DataImpl data = new DataImpl();
+		data.reset();
+		
+		try {
+			data.createEmployee("lisa", "romita");
+		
+			int balance = data.getEmployeeBalance(1);
+			int value = data.rechargeAccount(1, 100);
+			assertEquals(100 + balance,value);
+		
+		}catch(EmployeeException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void testRechargeException() throws EmployeeException {
+		
+		DataImpl data = new DataImpl();
+		data.reset();		
+		
+		try {
+			data.rechargeAccount(2, 500);
+		}catch (EmployeeException e) {
+			assertEquals("ID of the employee is not valid", e.getMessage());
+		}
+		
+		
+	}
+	
+	@Test
+	public void testRechargeWithZero() throws EmployeeException{
+		
+		DataImpl data = new DataImpl();
+		data.reset();
+		
+		try {
+			data.createEmployee("lisa", "romita");
+		
+			int balance = data.getEmployeeBalance(1);
+			int value = data.rechargeAccount(1, 0);
+			assertEquals(balance,value);
+		
+		}catch(EmployeeException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void testRechargeMAXINT() throws EmployeeException{
+		
+		DataImpl data = new DataImpl();
+		data.reset();
+		
+		try {
+			data.createEmployee("lisa", "romita");
+		
+			int balance = data.getEmployeeBalance(1);
+			int value = data.rechargeAccount(1, Integer.MAX_VALUE);
+			assertEquals(Integer.MAX_VALUE + balance,value);
+		
+		}catch(EmployeeException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
