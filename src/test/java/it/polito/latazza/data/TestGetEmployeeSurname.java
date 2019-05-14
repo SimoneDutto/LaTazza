6bc427d05442af68511b16a57afbf3d2661d9240
s263626 <s263626@studenti.polito.it>
2019-05-14 17:55:27 +0200

    Add JUnit tests for the method getEmployeeSurname() in DataImpl

---
+++ src/test/java/it/polito/latazza/data/TestGetEmployeeSurname.java
@@ -0,0 +1,61 @@
+package it.polito.latazza.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import it.polito.latazza.exceptions.EmployeeException;
+
+public class TestGetEmployeeSurname {
+	
+	@Test
+	public void testValidInputs() throws EmployeeException {
+		Integer employeeId;
+		String surname;
+		DataImpl data = new DataImpl();
+		data.reset();
+		employeeId = data.createEmployee("debora", "caldarola");
+		surname = data.getEmployeeSurname(employeeId);
+		assertEquals(surname, "caldarola");
+	}
+	
+	@Test
+	public void testNegativeId() {
+		DataImpl data = new DataImpl();
+		data.reset();
+		try {
+			data.createEmployee("debora", "caldarola");
+			data.getEmployeeSurname(-1);
+			assert(false);
+		} catch (EmployeeException e) {
+			assertEquals("ID of the employee is not valid", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testNullId() {
+		DataImpl data = new DataImpl();
+		data.reset();
+		try {
+			data.createEmployee("debora", "caldarola");
+			data.getEmployeeSurname(0);
+			assert(false);
+		} catch (EmployeeException e) {
+			assertEquals("ID of the employee is not valid", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testOutOfMaxBoundaryId() {
+		DataImpl data = new DataImpl();
+		Integer employeeId;
+		data.reset();
+		try {
+			employeeId = data.createEmployee("debora", "caldarola");
+			data.getEmployeeSurname(employeeId+1);
+			assert(false);
+		} catch (EmployeeException e) {
+			assertEquals("ID of the employee is not valid", e.getMessage());
+		}
+	}
+
+}
