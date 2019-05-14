33c9963f4a5ec0dc72a935f5279a78fb59485aba
Vito Tassielli <s263006@studenti.polito.it>
2019-05-14 15:16:14 +0000

    Upload TestGetBeverageCapsulesPerBox.java

---
+++ src/test/java/it/polito/latazza/data/TestGetBeverageCapsulesPerBox.java
@@ -0,0 +1,33 @@
+package it.polito.latazza.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import it.polito.latazza.exceptions.BeverageException;
+
+class TestGetBeverageCapsulesPerBox {
+	
+	@Test
+	public void testBeverageIdNotValid(){
+		DataImpl data = new DataImpl();
+		data.reset();
+		try {
+			data.createBeverage("coffee", 50, 10);
+			data.getBeverageCapsulesPerBox(2);
+		}
+		catch(BeverageException e) {
+			assertEquals("ID of the beverage is not valid", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testGetCapsulesPerBoxValid() throws BeverageException{
+		DataImpl data = new DataImpl();
+		data.reset();
+		
+		data.createBeverage("coffee", 50, 10);
+		data.createBeverage("the", 30, 20);
+		assert(data.getBeverageCapsulesPerBox(1) == 50);
+		assert(data.getBeverageCapsulesPerBox(2) == 30);
+	}
+}
